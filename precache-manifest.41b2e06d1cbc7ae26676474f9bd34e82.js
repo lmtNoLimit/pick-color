dd40fdb8f155b3b729d57d4b3743fde0e0d487fb
Lam Thanh <lmt151099@gmail.com>
2020-03-30 00:30:42 +0700

    Updates

---
+++ precache-manifest.41b2e06d1cbc7ae26676474f9bd34e82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8f00c333e6ceb2e5b795c107c18e0e",
+    "url": "/pick-color/index.html"
+  },
+  {
+    "revision": "6f90db494602bbc6cfba",
+    "url": "/pick-color/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "70cc78479d28e7c82320",
+    "url": "/pick-color/static/css/main.16e02298.chunk.css"
+  },
+  {
+    "revision": "6f90db494602bbc6cfba",
+    "url": "/pick-color/static/js/2.f8023938.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pick-color/static/js/2.f8023938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70cc78479d28e7c82320",
+    "url": "/pick-color/static/js/main.dcb73953.chunk.js"
+  },
+  {
+    "revision": "368e6e500a9abd7b9599",
+    "url": "/pick-color/static/js/runtime-main.bb5220df.js"
+  },
+  {
+    "revision": "a6869d367e5e43e6c6f52ff9cb224144",
+    "url": "/pick-color/static/media/intro.a6869d36.svg"
+  }
+]);
